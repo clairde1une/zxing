5dba37bc942e4d5d8312b0ceea3446e336d0f817
pavlobabenko <pavlo.babenko@nure.ua>
2017-03-15 22:05:10 +0200

    minor refactoring of MainActivity

--- app/src/main/java/ru/myapp/zxing3/MainActivity.java
+++ app/src/main/java/ru/myapp/zxing3/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.myapp.zxing3;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,46 +13,48 @@ import me.sudar.zxingorient.ZxingOrientResult;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvresult;
-    Button btn;
+    private static final String RESULT = "result";
+
+    private TextView textViewResult;
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvresult = (TextView) findViewById(R.id.result);
-        btn = (Button) findViewById(R.id.btn_scan);
-        btn.setOnClickListener(new View.OnClickListener() {
+        textViewResult = (TextView) findViewById(R.id.result);
+        scanButton = (Button) findViewById(R.id.btn_scan);
+        scanButton.setOnClickListener(getOnClickListener());
+    }
+
+    @NonNull
+    private View.OnClickListener getOnClickListener() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new ZxingOrient(MainActivity.this).initiateScan();
             }
-        });
-
+        };
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-
-        ZxingOrientResult scanResult =
-                ZxingOrient.parseActivityResult(requestCode, resultCode, intent);
-
+        ZxingOrientResult scanResult = ZxingOrient.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
-            tvresult.setText(scanResult.getContents());
+            textViewResult.setText(scanResult.getContents());
         }
-
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString("result", (String) tvresult.getText());
+        outState.putString(RESULT, (String) textViewResult.getText());
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        tvresult.setText(savedInstanceState.getString("result"));
+        textViewResult.setText(savedInstanceState.getString(RESULT));
     }
 }
