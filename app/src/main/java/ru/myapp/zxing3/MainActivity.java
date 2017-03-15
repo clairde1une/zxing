c9fe73e5f175687751f7d96bcf1cfc8f782be796
pavlobabenko <pavlo.babenko@nure.ua>
2017-03-15 20:00:26 +0200

    Test

---
+++ app/src/main/java/ru/myapp/zxing3/MainActivity.java
@@ -0,0 +1,57 @@
+package ru.myapp.zxing3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import me.sudar.zxingorient.ZxingOrient;
+import me.sudar.zxingorient.ZxingOrientResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvresult;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvresult = (TextView) findViewById(R.id.result);
+        btn = (Button) findViewById(R.id.btn_scan);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ZxingOrient(MainActivity.this).initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        ZxingOrientResult scanResult =
+                ZxingOrient.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanResult != null) {
+            tvresult.setText(scanResult.getContents());
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("result", (String) tvresult.getText());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        tvresult.setText(savedInstanceState.getString("result"));
+    }
+}
